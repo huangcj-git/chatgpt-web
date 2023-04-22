76304c5700dddc3ed10d0e31fd13f45654fa8deb
jun14111 <1411187249@qq.com>
2023-04-22 23:44:23 +0800

    feat: 配置代理

--- config/proxy.ts
+++ config/proxy.ts
@@ -5,6 +5,11 @@ export function createViteProxy(isOpenProxy: boolean, viteEnv: ImportMetaEnv) {
     return
 
   const proxy: Record<string, string | ProxyOptions> = {
+    '/api/translate': {
+      target: 'http://127.0.0.1:18888/',
+      changeOrigin: true,
+      rewrite: path => path.replace('/api/translate', '/translate'),
+    },
     '/api': {
       target: viteEnv.VITE_APP_API_BASE_URL,
       changeOrigin: true,
--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,12 @@ export default defineConfig((env) => {
       port: 1002,
       open: false,
       proxy: {
+        '/api/translate': {
+          target: 'http://127.0.0.1:18888/',
+          changeOrigin: true,
+          rewrite: path => path.replace('/api/translate', '/translate'),
+        },
+
         '/api': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
